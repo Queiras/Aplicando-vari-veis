ecec9a818d28d43a26a6ee1ece89154179cac463
Jorge Felipe <queirozprog@gmail.com>
2024-06-12 18:19:53 -0300

    Treinamento de variáveis

---
+++ Treinamento.js
@@ -0,0 +1,5 @@
+let facul = prompt("Qual a sua faculdade?")
+let tempo = prompt("Faz a quanto tempo?")
+let gostar = prompt("Você está gostando?")
+
+console.log("Faço faculdade de " + facul + " tenho " + tempo + " anos " + " e " + gostar +  " estou gostando mt de cursa-la")
